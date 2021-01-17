8958db6b61132b29ee8c7a0984ddc05f7dbbed7e
Aleksandr Bernikov <bernikov_1993@mail.ru>
2021-01-17 17:01:05 +0500

    Initial commit

---
+++ src/com/bernikov/Cell.java
@@ -0,0 +1,15 @@
+package com.bernikov;
+
+public class Cell {
+    char field,fieldDraw;
+
+    public Cell() { }
+
+    public void setField(char field) {
+        this.field = field;
+    }
+
+    public char getField() {
+        return this.field;
+    }
+}
